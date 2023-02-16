5bf2a21df980364ffddc87a0629db3634e80dfe7
bibidi77 <fullmoon7813@gmail.com>
2023-02-16 13:18:49 +0900

    Test message

--- server/be/src/main/java/com/codestates/be/BeApplication.java
+++ server/be/src/main/java/com/codestates/be/BeApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
 	}
-
+	//Hello Freind
 }
