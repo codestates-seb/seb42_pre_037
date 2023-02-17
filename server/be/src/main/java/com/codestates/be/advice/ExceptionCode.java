9e2e022b9460bd00fd7f099a4501f666af7d23a7
bibidi77 <fullmoon7813@gmail.com>
2023-02-18 07:01:01 +0900

    feat : ExceptionCode.java 추가 feat : MemberController.java 추가, 임시 핸들러 할당 feat : Member.java 추가, 엔티티 간 연결 필요

--- server/be/src/main/java/com/codestates/be/advice/ExceptionCode.java
+++ server/be/src/main/java/com/codestates/be/advice/ExceptionCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다."),
-    MEMBER_ALREADY_EXISTS(406, "이미 가입한 회원입니다.");
+    MEMBER_ALREADY_EXISTS(406, "이미 가입한 회원입니다."),
+    SERVICE_NOT_READY(404, "아직 구현되지 않았습니다.");
     private int httpStatus;
     private String message;
 
