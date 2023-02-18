d7b14aa288a49aaeedb04fa54b6a0188d737b628
codnr <wnd75913@gmail.com>
2023-02-19 03:04:18 +0900

    feat: 공통 UI Input 컴포넌트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,34 @@ import Button from './Components/UI/Button';
 import SearchBar from './Components/UI/SearchBar';
 import Login from './Pages/Login';
 import Card from './Components/UI/Card';
+import Input from './Components/UI/Input';
 
 function App() {
   return (
     <div>
       <Login />
-      <Card className=" m-5 space-x-2">
-        <Button color="clear-blue" size="large" type="button">
-          Log in
-        </Button>
-        <Button color="blue" size="medium" type="button">
-          Sign up
-        </Button>
-        <Button color="green" size="small" type="button">
-          버튼입니다!
-        </Button>
-      </Card>
-      <Card className="m-5 flex flex-col mt-2 space-y-2">
-        <SearchBar placeholder="Filter by tag name..." />
-        <SearchBar placeholder="Search..." />
-      </Card>
+      <div className="flex flex-col justify-center space-y-5">
+        <Card className="mx-auto space-x-2">
+          <Button color="clear-blue" size="large" type="button">
+            Log in
+          </Button>
+          <Button color="blue" size="medium" type="button">
+            Sign up
+          </Button>
+          <Button color="green" size="small" type="button">
+            버튼입니다!
+          </Button>
+        </Card>
+        <Card className="mx-auto flex flex-col mt-2 space-y-2">
+          <SearchBar placeholder="Filter by tag name..." />
+          <SearchBar placeholder="Search..." />
+        </Card>
+        <Card className="mx-auto space-y-2">
+          <Input label="Display name" placeholder="Display name" />
+          <Input label="Email" placeholder="Email" disabled />
+          <Input label="Password" placeholder="Password" />
+        </Card>
+      </div>
     </div>
   );
 }
