dced4f27c6b7f9c4c22fb7ebe552c94794b05956
codnr <wnd75913@gmail.com>
2023-02-19 00:48:45 +0900

    feat: Card 컴포넌트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import Button from './Components/UI/Button';
 import SearchBar from './Components/UI/SearchBar';
 import Login from './Pages/Login';
+import Card from './Components/UI/Card';
 
 function App() {
   return (
     <div>
       <Login />
-      <div className="m-5 space-x-2">
+      <Card className=" m-5 space-x-2">
         <Button color="gray" size="large" type="button">
           버튼입니다!
         </Button>
@@ -16,11 +17,11 @@ function App() {
         <Button color="green" size="small" type="button">
           버튼입니다!
         </Button>
-      </div>
-      <div className="m-5 flex flex-col mt-2 space-y-2 w-96">
+      </Card>
+      <Card className="m-5 flex flex-col mt-2 space-y-2">
         <SearchBar placeholder="Filter by tag name..." />
         <SearchBar placeholder="Search..." />
-      </div>
+      </Card>
     </div>
   );
 }
