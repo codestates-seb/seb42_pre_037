a0c8e19ef8c19472df6fd1143949ccb2e91d7db5
codnr <wnd75913@gmail.com>
2023-02-18 21:49:53 +0900

    refactor: searchBar 컨포넌트에서 중복 요소 및 불필요한 코드 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Button from './Components/UI/Button';
+import SearchBar from './Components/UI/SearchBar';
 import Login from './Pages/Login';
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
       <Button color="gray" size="large" type="button">
         버튼입니다!
       </Button>
+      <div className="flex flex-col mt-2 space-y-2 w-96">
+        <SearchBar placeholder="Filter by tag name" />
+        <SearchBar placeholder="Search..." />
+      </div>
     </div>
   );
 }
--- client/src/Components/Ui/SearchBar.js
+++ client/src/Components/Ui/SearchBar.js
@@ -1,5 +1,16 @@
-function SearchBar() {
-  return <div />;
+function SearchBar({ placeholder, ...rest }) {
+  // 크기에 따른 클래스 이름 결정
+
+  const className = `inline-flex items-center justify-center font-semibold border-2 focus:outline-none bg-white focus:ring-2 focus:ring-opacity-50 disabled:opacity-50 disabled:pointer-events-none text-sm px-4 py-2 rounded `;
+
+  return (
+    <input
+      type="text"
+      className={className}
+      placeholder={placeholder}
+      {...rest}
+    />
+  );
 }
 
 export default SearchBar;
