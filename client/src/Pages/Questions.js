da1ce2bcda08c627b76c00847dbc072b3a6ba2ce
codnr <wnd75913@gmail.com>
2023-02-20 03:27:43 +0900

    feat: paginate 적용

--- client/src/Pages/Questions.js
+++ client/src/Pages/Questions.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { BiFilter } from 'react-icons/bi';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 import QuestionsItem from '../Components/QuestionsItem';
 import Button from '../Components/UI/Button';
@@ -9,12 +10,18 @@ import dummyData from '../dummyData';
 function Questions() {
   const [questions, setQuestions] = useState(dummyData.data);
   const [totalQuestion, setTotalElements] = useState(dummyData.pageInfo);
+  // currentPage 초기값은 0으로 설정
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const PER_PAGE = 10;
+  // page 갯수 계산
+  const pageCount = Math.ceil(totalQuestion.totalElements / PER_PAGE);
 
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
         const response = await axios.get(
-          'http://test:8080/questions?page=1&size=10',
+          `http://test:8080/questions?page=${currentPage}&size=${PER_PAGE}`,
         );
         setQuestions(response.data);
         setTotalElements(response.pageInfo);
@@ -23,7 +30,12 @@ function Questions() {
       }
     };
     fetchQuestions();
-  }, []);
+  }, [currentPage]);
+  // currentPage가 변경될 때 마다 API호출을 한다.
+
+  const handlerPageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
 
   return (
     <>
@@ -51,6 +63,21 @@ function Questions() {
             <QuestionsItem key={question.questionId} question={question} />
           ))}
         </ul>
+      </div>
+
+      {/* Pagination */}
+      <div className="flex justify-center pt-5">
+        <ReactPaginate
+          previousLabel="prev"
+          nextLabel="next"
+          breakLabel="..."
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlerPageClick}
+          containerClassName="bg-blue-400"
+          activeClassName="bg-blue-400"
+        />
       </div>
     </>
   );
