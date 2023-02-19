e5312c9de7ffdb56f067f0e581419b98c944c039
codnr <wnd75913@gmail.com>
2023-02-19 17:14:55 +0900

    feat: Button 컴포넌트에 icon 넣는 기능 추가

--- client/src/Pages/Questions.js
+++ client/src/Pages/Questions.js
@@ -1,5 +1,27 @@
+import { BiFilter } from 'react-icons/bi';
+
+import Button from '../Components/UI/Button';
+
 function Questions() {
-  return <div>Questions</div>;
+  return (
+    <>
+      <div>
+        <h1>All Questions</h1>
+        <Button>Ask Question</Button>
+      </div>
+      <div>
+        <h3>Questions</h3>
+        <Button
+          color="clear-blue"
+          icon={<BiFilter />}
+          iconClassName="text-2xl"
+          size="small"
+        >
+          Filter
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default Questions;
