7c3b87cfed22e6fdc0baabd1365a648134a9414a
bibidi77 <fullmoon7813@gmail.com>
2023-02-20 21:40:13 +0900

    feat: 시큐리티 기본 세팅 구현

--- server/be/src/main/java/com/codestates/be/security/verification/filter/JwtVerificationFilter.java
+++ server/be/src/main/java/com/codestates/be/security/verification/filter/JwtVerificationFilter.java
@@ -2,6 +2,14 @@ package com.codestates.be.security.verification.filter;
 
 import com.codestates.be.security.jwt.JwtTokenizer;
 import com.codestates.be.security.util.CustomAuthorityUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +17,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class JwtVerificationFilter extends OncePerRequestFilter {
     private final JwtTokenizer jwtTokenizer;
@@ -23,13 +33,41 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
+        try{
+            Map<String, Object> claims = verifyJws(request);
+            setSecurityContext(claims);
+        }catch (SignatureException se){
+            request.setAttribute("exception", se);
+        }catch (ExpiredJwtException ee){
+            request.setAttribute("exception", ee);
+        }catch (Exception e){
+            request.setAttribute("exception", e);
+        }
+        filterChain.doFilter(request, response);
     }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return super.shouldNotFilter(request);
+        String authorization = request.getHeader("Authorization");
+
+        return authorization == null || !authorization.startsWith("Bearer");
     }
 
+    public Map<String, Object> verifyJws(HttpServletRequest request){
+        String jws = request.getHeader("Authorization").replace("Bearer ", "");
+        Map<String, Object> claims = jwtTokenizer.getClaims(jws, jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey()));
+
+        return claims;
+    }
+
+    public void setSecurityContext(Map<String , Object> claims){
+        String username = (String) claims.get("username");
+        List<GrantedAuthority> authorities =
+                authorityUtil.createAuthorities((List) claims.get("roles"));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 
 }
