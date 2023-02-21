70179a59e4712b491d1ff55e82a2f556ffde352c
codnr <wnd75913@gmail.com>
2023-02-22 03:12:38 +0900

    feat:quill toolbar 설정

--- client/src/Components/Ui/TextEditor.js
+++ client/src/Components/Ui/TextEditor.js
@@ -1,8 +1,22 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ align: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    ['clean'],
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+};
+
 function TextEditor() {
-  return <ReactQuill />;
+  return <ReactQuill them="snow" modules={modules} />;
 }
 
 export default TextEditor;
