4982cae2e850f25081d9449029428f440c7f9921
puuruun <puuruun@gmail.com>
2023-02-21 17:35:32 +0900

    feat : answer 패키지 관련
    feat : Answer 관련 추가 작업 중

--- server/be/src/main/java/com/codestates/be/answer/service/AnswerService.java
+++ server/be/src/main/java/com/codestates/be/answer/service/AnswerService.java
@@ -4,21 +4,26 @@ package com.codestates.be.answer.service;
 import com.codestates.be.advice.BuissnessLogicException;
 import com.codestates.be.advice.ExceptionCode;
 import com.codestates.be.answer.entity.Answer;
+import com.codestates.be.answer.repository.AnswerRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class AnswerService {
+    private final AnswerRepository answerRepository;
 
+    public AnswerService(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
     // 답변 등록
     public Answer createAnswer(Answer answer) {
 
         //Answer createdAnswer = answer;
         //return createdAnswer;
 
-        return null;
-//        throw new BuissnessLogicException(ExceptionCode.SERVICE_NOT_READY);
+        Answer response = answerRepository.save(answer);
+        return response;
     }
 
     // 답변 수정
@@ -27,20 +32,33 @@ public class AnswerService {
         //Answer updatedAnswer = answer;
         //return updatedAnswer;
 
-        return null;
-//        throw new BuissnessLogicException(ExceptionCode.SERVICE_NOT_READY);
+        // 답변 있는지 확인
+        Answer existAnswer = findVerifiedExistAnswer(answer.getAnswerId());
+
+        Answer response = answerRepository.save(existAnswer);
+        return response;
     }
 
     // 답변 전체 조회
-    public List<Answer> findAnswers() {
+    public List<Answer> findAnswers(long questionId) {
 
-        return null;
-//        throw new BuissnessLogicException(ExceptionCode.SERVICE_NOT_READY);
+        List<Answer> response = findVerifiedExistAnswer(questionId);
+        return response;
     }
 
     // 답변 삭제
     public void deleteAnswer(long answerId) {
 
-//        throw new BuissnessLogicException(ExceptionCode.SERVICE_NOT_READY);
+        Answer existAnswer = findVerifiedExistAnswer(answerId);
+        answerRepository.delete(existAnswer);
+    }
+
+    // 예외 처리
+    private Answer findVerifiedExistAnswer(long questionId) {
+        Optional<Answer> optionalAnswer = answerRepository.findById(questionId);
+        Answer existAnswer = optionalAnswer.orElseThrow(
+                () -> new BuissnessLogicException(ExceptionCode.ANSWER_NOT_FOUND)
+        );
+        return existAnswer;
     }
 }
