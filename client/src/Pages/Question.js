983e903fb7f255e8aa1a93e28ed761c062bd499e
codnr <wnd75913@gmail.com>
2023-02-22 02:40:43 +0900

    style: question style 수정

--- client/src/Pages/Question.js
+++ client/src/Pages/Question.js
@@ -7,6 +7,7 @@ function Question() {
   const navigate = useNavigate();
   const location = useLocation();
   const { question } = location.state;
+  const timeDiff = getTimeDiffString(question.createAt);
 
   const handlerChangeQuestion = () => {
     navigate('/question/ask');
@@ -21,11 +22,30 @@ function Question() {
           Ask Question
         </Button>
       </div>
-      <div className="flex space-x-2 py-3 border-b">
-        <p className="text-gray-400 ">Asked</p>
-        <p>{getTimeDiffString(question.createAt)}</p>
+
+      <div className="flex space-x-2 py-3 border-b mb-4">
+        <p className="text-gray-400">Asked</p>
+        <p>{timeDiff}</p>
       </div>
+
       <div>{question.content}</div>
+
+      <div className="mt-5 flex justify-between">
+        <div className="flex jus space-x-3 text-gray-500">
+          <p>Share</p>
+          <p>Edit</p>
+          <p>Follow</p>
+        </div>
+
+        <div className="p-2 w-48 rounded clear-blue">
+          <div className="text-gray-500 text-sm">edit {timeDiff}</div>
+
+          <div className="flex h-10">
+            <img className="bg-white" src="img/avatar.png" alt="avatar" />
+            <p className="h-blue ml-2 text-sm">{question.displayName}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
